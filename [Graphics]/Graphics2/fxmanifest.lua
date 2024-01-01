d8280ddea4a569e974f6d78fa1a82fc6c03ad7e7
Nixity <93635940+Howlwh@users.noreply.github.com>
2024-01-01 14:32:23 -0800

    W@
    @

---
+++ [Graphics]/Graphics2/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Specify the resource manifest
+fx_version 'cerulean'
+games { 'gta5' }
+
+files {
+    'stream/*.xml',
+    'stream/content/*.ytd',
+    'stream/content/*.ydr',
+    'stream/content/*.ytyp'
+}
